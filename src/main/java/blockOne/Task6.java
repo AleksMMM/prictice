69f071068c6f72c5e11a7406c0084fc08554e217
John Doe <girbov@gmail.com>
2022-02-13 22:11:56 +0300

    task6

---
+++ src/main/java/blockOne/Task6.java
@@ -0,0 +1,16 @@
+package blockOne;
+
+/**
+ * 8. Удаление пробелов из строки
+ */
+
+public class Task6 {
+    public static void main(String[] args) {
+
+    }
+
+    public static String removewhitespaces(String str) {
+        return str.replaceAll("\\s", "");
+    }
+
+}
